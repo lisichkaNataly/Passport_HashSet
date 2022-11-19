95947d3bfe71b55c916c49ca5a6eae237d7e7638
Izmaylova <Natalyatf@yandex.ru>
2022-11-20 00:19:24 +0500

    Паспорт: Домашнее задание 3 Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Passport ivan = new Passport(
+                Collections.singleton("523HQG"),
+                "Иванов",
+                "Иван",
+                "Иванович", "01.01.1980");
+        Passport sergei = new Passport(
+                Collections.singleton("184AEW"),
+                "Сергеев",
+                "Сергей",
+                "Сергеевич",
+                "02.02.1957");
+        Passport andrei = new Passport(
+                Collections.singleton("591KРB"),
+                "Андреев",
+                "Андрей",
+                "Андреевич",
+                "03.03.2000");
+
+        Set<Passport> passportSets = new HashSet<>();
+        passportSets.add(ivan);
+        passportSets.add(sergei);
+        passportSets.add(andrei);
+
+        Iterator itr = passportSets.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+        System.out.println("Данные паспорта:" + find(passportSets, "591KРB"));
+
+
     }
+
+    public static Passport find(Set<Passport> passportSets, String number) {
+        for (Passport passports: passportSets) {
+            if (passports.isSetNumber(number)) {
+                return passports;
+            }
+        }
+        return null;
+    }
+
+
+
+
 }
